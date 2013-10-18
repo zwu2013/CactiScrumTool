66152a284ddc6353139aefbc80f214d4032d8711
jackliao <jkliao@gmail.com>
2013-10-17 22:55:20 -0400

    Some tests using SmartGWT

--- cacti-scrumtool-webapp-1.0.0/src/main/java/org/cacti/scrumtool/client/CactiScrumTool.java
+++ cacti-scrumtool-webapp-1.0.0/src/main/java/org/cacti/scrumtool/client/CactiScrumTool.java
@@ -2,20 +2,12 @@ package org.cacti.scrumtool.client;
 
 import org.cacti.scrumtool.shared.FieldVerifier;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
+
+
+import com.smartgwt.client.widgets.layout.HLayout;  
+import com.smartgwt.client.widgets.layout.VLayout;  
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -36,122 +28,46 @@ public class CactiScrumTool implements EntryPoint {
   private static final String SERVER_ERROR_III = "An error occurred while "
 	      + "attempting to contact the server. Please check your network "
 	      + "connection and try again.";
+  
+  private static final int HEADER_HEIGHT = 85;
 
-  /**
-   * Create a remote service proxy to talk to the server-side Greeting service.
-   */
-  private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
+  private VLayout mainLayout;
+  private HLayout northLayout;
+  private HLayout southLayout;
+  private VLayout eastLayout;
+  private HLayout westLayout;
+      
+  public void onModuleLoad() 
+  {
+      Window.enableScrolling(false);
+      Window.setMargin("0px");
+       
+      // main layout occupies the whole area
+      mainLayout = new VLayout();
+      mainLayout.setWidth100();
+      mainLayout.setHeight100();
 
-  private final Messages messages = GWT.create(Messages.class);
+      northLayout = new HLayout();
+      northLayout.setHeight(HEADER_HEIGHT);
 
-  /**
-   * This is the entry point method.
-   */
-  public void onModuleLoad() {
-    final Button sendButton = new Button( messages.sendButton() );
-    final TextBox nameField = new TextBox();
-    nameField.setText( messages.nameField() );
-    final Label errorLabel = new Label();
+      VLayout vLayout = new VLayout();
+      vLayout.addMember(new HeaderArea());
+      vLayout.addMember(new ApplicationMenu());
+      northLayout.addMember(vLayout);
 
-    // We can add style names to widgets
-    sendButton.addStyleName("sendButton");
+      westLayout = new NavigationArea();
+      westLayout.setWidth("15%");
+       
+      eastLayout = new MainArea();
+      eastLayout.setWidth("85%");
 
-    // Add the nameField and sendButton to the RootPanel
-    // Use RootPanel.get() to get the entire body element
-    RootPanel.get("nameFieldContainer").add(nameField);
-    RootPanel.get("sendButtonContainer").add(sendButton);
-    RootPanel.get("errorLabelContainer").add(errorLabel);
-
-    // Focus the cursor on the name field when the app loads
-    nameField.setFocus(true);
-    nameField.selectAll();
-
-    // Create the popup dialog box
-    final DialogBox dialogBox = new DialogBox();
-    dialogBox.setText("Remote Procedure Call");
-    dialogBox.setAnimationEnabled(true);
-    final Button closeButton = new Button("Close");
-    // We can set the id of a widget by accessing its Element
-    closeButton.getElement().setId("closeButton");
-    final Label textToServerLabel = new Label();
-    final HTML serverResponseLabel = new HTML();
-    VerticalPanel dialogVPanel = new VerticalPanel();
-    dialogVPanel.addStyleName("dialogVPanel");
-    dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-    dialogVPanel.add(textToServerLabel);
-    dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-    dialogVPanel.add(serverResponseLabel);
-    dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-    dialogVPanel.add(closeButton);
-    dialogBox.setWidget(dialogVPanel);
-
-    // Add a handler to close the DialogBox
-    closeButton.addClickHandler(new ClickHandler() {
-      public void onClick(ClickEvent event) {
-        dialogBox.hide();
-        sendButton.setEnabled(true);
-        sendButton.setFocus(true);
-      }
-    });
-
-    // Create a handler for the sendButton and nameField
-    class MyHandler implements ClickHandler, KeyUpHandler {
-      /**
-       * Fired when the user clicks on the sendButton.
-       */
-      public void onClick(ClickEvent event) {
-        sendNameToServer();
-      }
-
-      /**
-       * Fired when the user types in the nameField.
-       */
-      public void onKeyUp(KeyUpEvent event) {
-        if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-          sendNameToServer();
-        }
-      }
-
-      /**
-       * Send the name from the nameField to the server and wait for a response.
-       */
-      private void sendNameToServer() {
-        // First, we validate the input.
-        errorLabel.setText("");
-        String textToServer = nameField.getText();
-        if (!FieldVerifier.isValidName(textToServer)) {
-          errorLabel.setText("Please enter at least four characters");
-          return;
-        }
-
-        // Then, we send the input to the server.
-        sendButton.setEnabled(false);
-        textToServerLabel.setText(textToServer);
-        serverResponseLabel.setText("");
-        greetingService.greetServer(textToServer, new AsyncCallback<String>() {
-          public void onFailure(Throwable caught) {
-            // Show the RPC error message to the user
-            dialogBox.setText("Remote Procedure Call - Failure");
-            serverResponseLabel.addStyleName("serverResponseLabelError");
-            serverResponseLabel.setHTML(SERVER_ERROR);
-            dialogBox.center();
-            closeButton.setFocus(true);
-          }
-
-          public void onSuccess(String result) {
-            dialogBox.setText("Remote Procedure Call");
-            serverResponseLabel.removeStyleName("serverResponseLabelError");
-            serverResponseLabel.setHTML(result);
-            dialogBox.center();
-            closeButton.setFocus(true);
-          }
-        });
-      }
-    }
-
-    // Add a handler to send the name to the server
-    MyHandler handler = new MyHandler();
-    sendButton.addClickHandler(handler);
-    nameField.addKeyUpHandler(handler);
+      southLayout = new HLayout();
+      southLayout.setMembers(westLayout, eastLayout);
+      
+      mainLayout.addMember(northLayout);
+      mainLayout.addMember(southLayout);
+      
+      // add the main layout container to GWT's root panel
+      RootLayoutPanel.get().add(mainLayout);
   }
 }
