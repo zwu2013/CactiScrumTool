138d11456c2dc82d1d733cfc6b84b9c6d46ae729
zwu <zwu@192.168.1.102>
2014-05-23 13:20:39 -0400

    Test Unit Case

---
+++ cacti-scrumtool-webapp-1.0.0/src/test/java/org/cacti/scrumtool/server/dao/impl/SprintDAOImplTest.java
@@ -0,0 +1,20 @@
+package org.cacti.scrumtool.server.dao.impl;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class SprintDAOImplTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testII() {
+		assertTrue(true);
+	}
+
+}
